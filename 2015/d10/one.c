07a9d1eb10edf1dd20ea8f6137b118b24eecce57
Anna <annabeths111@gmail.com>
2022-02-12 22:08:15 +0000

    completed 2015/d10 and 2015/d11

---
+++ 2015/d10/one.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFERLENGTH 300000
+
+char* backBuffer;
+char* frontBuffer;
+
+
+void doStep();
+
+int main()
+{
+	backBuffer = calloc(BUFFERLENGTH, sizeof(char));
+	frontBuffer = calloc(BUFFERLENGTH, sizeof(char));
+	char* inputString = "1113222113";
+	/* char* inputString = "111221"; */
+	strcpy(frontBuffer, inputString);
+	for(int i=0; i < 40; i++)
+	{
+		doStep();
+	}
+	unsigned long x = strlen(frontBuffer);
+	printf("%lu\n", x);
+}
+
+void doStep()
+{
+	char* ptr = frontBuffer;
+	char prevChar = *ptr;
+	int prevCharCount = 0;
+	while(prevChar != 0)
+	{
+		if(prevChar == *ptr)
+		{
+			prevCharCount++;
+		}
+		else
+		{
+			char section[3] = {0};
+			section[0] = prevCharCount + '0';
+			section[1] = prevChar;
+			prevChar = *ptr;
+			prevCharCount = 1;
+			strcat(backBuffer, section);
+		}
+		ptr++;
+	}
+	memset(frontBuffer, 0, sizeof(char)*BUFFERLENGTH);
+	strcpy(frontBuffer, backBuffer);
+	memset(backBuffer, 0, sizeof(char)*BUFFERLENGTH);
+}
---
+++ 2015/d11/two.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STRLEN 8
+
+void increment(char *pass, int pos);
+char isValid(char* pass);
+
+int main()
+{
+	char pass[STRLEN+1] = "hepxcrrq";
+	printf("%s\n", pass);
+	while(!isValid(pass))
+		increment(pass, 7);
+	printf("pass1: %s\n", pass);
+
+	increment(pass, 7);
+	while(!isValid(pass))
+		increment(pass, 7);
+	printf("pass2: %s\n", pass);
+}
+
+void increment(char* pass, int pos)
+{
+	if(pos < 0)
+		return;
+	pass[pos]++;
+	if(pass[pos] > 'z')
+	{
+		pass[pos] = 'a';
+		increment(pass, pos-1);
+	}
+}
+
+char isValid(char* pass)
+{
+	//rule 1 - password must contain straight of 3
+	char r1 = 0;
+	for(int i=0; i < STRLEN-2; i++)
+	{
+		if(pass[i]+1 == pass[i+1] && pass[i+1]+1 == pass[i+2])
+		{
+			r1 = 1;
+			break;
+		}
+	}
+	if(!r1)
+		return 0;
+
+	//rule 2 - password cannot contain i o l
+
+	for(int i=0; i < STRLEN; i++)
+	{
+		if(pass[i] == 'i' || pass[i] == 'o' || pass[i] == 'l')
+			return 0;
+	}
+
+	//rule 3 - 2 distinct overlapping pairs
+
+	char prevPair = 0;
+	char pairs = 0;
+	for(int i=0; i < STRLEN-1; i++)
+	{
+		if(pass[i]==pass[i+1] && pass[i] != prevPair)
+		{
+			pairs++;
+			prevPair = pass[i];
+			if(pairs==2)
+				break;
+		}
+	}
+
+	return pairs == 2;
+}
