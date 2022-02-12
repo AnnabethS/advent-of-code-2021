9701e93592969c28c04084b41d94ce2d55dde77f
AnnabethS <annabeths111@gmail.com>
2022-02-12 16:48:02 +0000

    begun 2015/d8

---
+++ 2015/d8/one.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUFLEN 50
+
+int main()
+{
+	FILE* fp = fopen("input.txt", "r");
+	char bufStr[BUFLEN] = {0};
+}
+
+int getStringDiff(char* str)
+{
+	int codeLength = strlen(str);
+}
