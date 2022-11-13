6237411a7194f69a5fd5ae182ec6f0f71a2ff00b
Anna <annabeths111@gmail.com>
2022-11-13 15:07:00 +0000

    2016/d05 part 1 but faster

--- 2016/d05/src/main.rs
+++ 2016/d05/src/main.rs
@@ -1,70 +1,74 @@
-use md5::{Digest, Md5};
+use crypto::{md5::Md5, digest::Digest};
 
-fn get_hexstring(input: &String, iter: u64) -> String {
-    let x = format!("{}{}", input, iter);
-    Md5::digest(x.as_bytes()).iter()
-        .map(|i| format!("{:#04x}", i).split_at(2).1.to_string())
-        .collect::<Vec<String>>()
-        .concat()
+fn next_hash(inp: &String, iter: &mut u64) -> String {
+    let mut hasher = Md5::new();
+    let mut output = [0 as u8; 16];
+    let binp = inp.as_bytes();
+    loop {
+        hasher.input(binp);
+        hasher.input(iter.to_string().as_bytes());
+
+        hasher.result(&mut output);
+
+        // let test_str:String = output.iter().map(|x| format!("{:#04x}", x).split_at(2).1.to_string()).collect();
+
+        let bitmask = 0xf0u8;
+
+        let cond = output[0] == 0 && output[1] == 0 && (output[2] & bitmask) == 0;
+
+        if cond {
+            return output.iter().map(|x| format!("{:#04x}", x).split_at(2).1.to_string()).collect();
+        }
+
+        hasher.reset();
+
+        *iter += 1;
+    }
 }
 
-// fn next_hash(inp: String, iter: u64) {
-//     let mut hasher = Md5::new();
-//     let mut output = [0; 16];
-//     hasher.chain_update(inp.as_bytes()).update(iter.to_string().as_bytes());
-//     hasher.finalize_into_reset(&mut output);
-// }
-
-fn part_1(input: String) -> String {
-    let mut found = 0;
+fn part_1(input: &String) -> String {
     let mut iter = 0;
     let mut output = String::new();
-    while found < 8 {
-        let hstr = get_hexstring(&input, iter);
-        if &hstr[0..5] == "00000" {
-            println!("found char {}", found+1);
-            output.push(hstr.chars().nth(5).unwrap());
-            found += 1;
-        }
+    for _i in 0..8 {
+        let hstr = next_hash(&input, &mut iter);
         iter += 1;
+        println!("found working hash with iter: {}", iter);
+        output.push(hstr.chars().nth(5).unwrap());
     }
 
     output
 }
 
-fn part_2(input: String) -> String {
-    let mut found = 0;
+
+fn part_2(input: &String) -> String {
     let mut iter = 0;
-    // let mut output = String::new();
-    let mut output:Vec<Option<char>> = vec!(None, None, None, None, None, None, None, None);
-    while found < 8 {
-        if iter % 100000 == 0 {
-            println!("iter {}", iter);
-        }
-        let hstr = get_hexstring(&input, iter);
-        if &hstr[0..5] == "00000" {
-            let c = hstr.chars().nth(5).unwrap();
-            if "01234567".contains(c) {
-                println!("foooooo");
-                output[c.to_digit(10).unwrap() as usize] = Some(hstr.chars().nth(6).unwrap());
-                found += 1;
+    let mut o = ['\0'; 8];
+    let mut found = 0;
+    while found < 8{
+        let hstr = next_hash(&input, &mut iter);
+        iter += 1;
+        println!("found working hash with iter: {}", iter);
+        let pos_char = hstr.chars().nth(5).unwrap();
+        if "01234567".contains(pos_char) {
+            println!("valid pos char: {}", pos_char);
+            if o[pos_char.to_digit(10).unwrap() as usize] != '\0' {
+                println!("found empty pos, inserting new.")
+
             }
         }
-        iter += 1;
     }
 
-    output.iter().map(|x| x.unwrap()).collect()
+    todo!()
 }
 
+
 fn main() {
-    let input = "abc".to_string();
-    // let input = "reyedfim".to_string();
+    // let input = "abc".to_string();
+    let input = "reyedfim".to_string();
 
-    // this solution is pretty inefficient, just gen the part you want to see
+    let p1 = part_1(&input);
+    println!("part 1: {}", p1);
 
-    // let p1:String = part_1(input);
-    // println!("part 1: {}", p1);
-
-    let p2:String = part_2(input);
-    println!("part 2: {}", p2);
+    // let p2:String = part_2(input);
+    // println!("part 2: {}", p2);
 }
