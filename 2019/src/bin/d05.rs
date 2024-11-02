c552bb58d3b4e607f16e762ec1da6b7859c7e73e
anna-singleton <annabeths111@gmail.com>
2024-11-02 00:19:09 +0000

    2019 d05 complete

--- 2019/src/bin/d05.rs
+++ 2019/src/bin/d05.rs
@@ -1,5 +1,43 @@
 use year_2019::IntCodeVM;
 
+fn part_1(raw_code: &str) -> Option<i64> {
+    let mut vm = IntCodeVM::builder(raw_code)
+        .with_input_queue(vec![1].into())
+        .build();
+    vm.execute();
+    return vm.get_last_output();
+}
+
+fn part_2(raw_code: &str) -> Option<i64> {
+    let mut vm = IntCodeVM::builder(raw_code)
+        .with_input_queue(vec![5].into())
+        .build();
+    vm.execute();
+    return vm.get_last_output();
+}
+
 fn main() {
-    unimplemented!();
+    let raw_code = std::fs::read_to_string("data/d05.txt").unwrap();
+    let p1 = part_1(&raw_code).unwrap_or(-1);
+    println!("part 1: {}", p1);
+
+    let p2 = part_2(&raw_code).unwrap_or(-1);
+    println!("part 2: {}", p2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_functional() {
+        let raw_code = std::fs::read_to_string("data/d05.txt").unwrap();
+        assert_eq!(9938601, part_1(&raw_code).unwrap_or(-1));
+    }
+
+    #[test]
+    fn part_2_functional() {
+        let raw_code = std::fs::read_to_string("data/d05.txt").unwrap();
+        assert_eq!(4283952, part_2(&raw_code).unwrap_or(-1));
+    }
 }
--- 2019/src/lib.rs
+++ 2019/src/lib.rs
@@ -78,8 +78,16 @@ impl IntCodeVM {
             2 => self.mult(&instruction.arg_modes),
             3 => self.input(&instruction.arg_modes),
             4 => self.output(&instruction.arg_modes),
+            5 => self.jump_if_true(&instruction.arg_modes),
+            6 => self.jump_if_false(&instruction.arg_modes),
+            7 => self.less_than(&instruction.arg_modes),
+            8 => self.equals(&instruction.arg_modes),
             99 => return false,
-            _ => unimplemented!()
+            _ => {
+                println!("Reached An Unknown Instruction ({})!", instruction.instruction);
+                self.dump_state();
+                unimplemented!();
+            }
         }
         return true;
     }
@@ -124,12 +132,83 @@ impl IntCodeVM {
         self.output_queue.push_back(num);
     }
 
+    fn jump_if_true(&mut self, arg_modes: &Vec<ParameterMode>) {
+        let (arg, arg2) = self.peek_2_args();
+        let cmp_num = self.get_int_for_param(arg, arg_modes[2]);
+        let to_num = self.get_int_for_param(arg2, arg_modes[1]);
+        if cmp_num != 0 {
+            self.pc = to_num;
+        } else {
+            self.pc += 3;
+        }
+    }
+
+    fn jump_if_false(&mut self, arg_modes: &Vec<ParameterMode>) {
+        let (arg, arg2) = self.peek_2_args();
+        let cmp_num = self.get_int_for_param(arg, arg_modes[2]);
+        let to_num = self.get_int_for_param(arg2, arg_modes[1]);
+        if cmp_num == 0 {
+            self.pc = to_num;
+        } else {
+            self.pc += 3;
+        }
+    }
+
+    fn less_than(&mut self, arg_modes: &Vec<ParameterMode>) {
+        let (arg, arg2, arg3) = self.take_3_args();
+        let cmp_1_num = self.get_int_for_param(arg, arg_modes[2]);
+        let cmp_2_num = self.get_int_for_param(arg2, arg_modes[1]);
+        if cmp_1_num < cmp_2_num {
+            self.set_int_at(arg3, 1);
+        } else {
+            self.set_int_at(arg3, 0);
+        }
+
+    }
+
+    fn equals(&mut self, arg_modes: &Vec<ParameterMode>) {
+        let (arg, arg2, arg3) = self.take_3_args();
+        let cmp_1_num = self.get_int_for_param(arg, arg_modes[2]);
+        let cmp_2_num = self.get_int_for_param(arg2, arg_modes[1]);
+        if cmp_1_num == cmp_2_num {
+            self.set_int_at(arg3, 1);
+        } else {
+            self.set_int_at(arg3, 0);
+        }
+    }
+
+
+    fn peek_3_args(&mut self) -> (i64, i64, i64) {
+        let arg1 = self.get_int_at(self.pc+1);
+        let arg2 = self.get_int_at(self.pc+2);
+        let arg3 = self.get_int_at(self.pc+3);
+        return (arg1, arg2, arg3);
+    }
+
+    fn peek_2_args(&mut self) -> (i64, i64) {
+        let arg1 = self.get_int_at(self.pc+1);
+        let arg2 = self.get_int_at(self.pc+2);
+        return (arg1, arg2);
+    }
+
+    fn peek_1_arg(&mut self) -> i64 {
+        let arg1 = self.get_int_at(self.pc+1);
+        return arg1;
+    }
+
     fn take_3_args(&mut self) -> (i64, i64, i64) {
         let arg1 = self.get_int_at(self.pc+1);
         let arg2 = self.get_int_at(self.pc+2);
         let arg3 = self.get_int_at(self.pc+3);
         self.pc += 4;
         return (arg1, arg2, arg3);
+    }
+
+    fn take_2_args(&mut self) -> (i64, i64) {
+        let arg1 = self.get_int_at(self.pc+1);
+        let arg2 = self.get_int_at(self.pc+2);
+        self.pc += 3;
+        return (arg1, arg2);
     }
 
     fn take_1_arg(&mut self) -> i64 {
@@ -155,7 +234,11 @@ impl IntCodeVM {
 
     pub fn dump_state(&self) {
         let mut output = String::new();
-        for (i, val) in self.memory.iter() {
+        let mut vec_mem: Vec<(i64, i64)> = self.memory.clone().iter()
+            .map(|(i, val)| (*i, *val))
+            .collect();
+        vec_mem.sort_by(|a, b| (*a).0.cmp(&b.0));
+        for (i, val) in vec_mem.iter() {
             if self.pc == *i as i64 {
                 output.push_str(&format!("[{}]: {} <--\n", i, val))
             } else {
@@ -163,6 +246,20 @@ impl IntCodeVM {
             }
         }
         println!("{}", output);
+        println!("pc: {}", self.pc);
+        println!("input queue: {:?}", self.input_queue);
+        println!("output queue: {:?}", self.output_queue);
+    }
+
+    pub fn dump_output_queue(&self) {
+        let mut queue_clone = self.output_queue.clone();
+        while let Some(out) = queue_clone.pop_front() {
+            println!("{}", out);
+        }
+    }
+
+    pub fn get_last_output (&self) -> Option<i64> {
+        return self.output_queue.back().copied();
     }
 }
 
@@ -184,6 +281,24 @@ mod tests {
             vm.dump_state();
             panic!("vm memory did not match expected state.");
         }
+    }
+
+    fn assert_expected_output(code: &Vec<i64>, input: Vec<i64>, expected_output: Vec<i64>) {
+        let mut vm = IntCodeVMBuilder::new(IntCodeVMBuilder::mem_from_vec(code.to_vec()))
+            .with_input_queue(input.into())
+            .build();
+        vm.execute();
+
+        let output_queue: Vec<i64> = vm.output_queue.clone().into();
+
+        if output_queue != expected_output {
+            println!("Expected: {:?}", expected_output);
+            println!("Got: {:?}", output_queue);
+            println!("VM State:");
+            vm.dump_state();
+            panic!("Output Queue did not match expected Output Queue.");
+        }
+        assert_eq!(expected_output, output_queue)
     }
 
     #[test]
@@ -239,6 +354,7 @@ mod tests {
         assert_eq!(expected, vm.output_queue);
     }
 
+
     #[test]
     fn test_instruction_parser_from_str() {
         let cases = vec![
@@ -265,4 +381,43 @@ mod tests {
         }
     }
 
+    #[test]
+    fn test_comparisons() {
+       let is_equal_to_8_pos_code = vec![3,9,8,9,10,9,4,9,99,-1,8];
+       let is_lt_8_pos_code = vec![3,9,7,9,10,9,4,9,99,-1,8];
+
+       let is_equal_to_8_im_code = vec![3,3,1108,-1,8,3,4,3,99];
+       let is_lt_8_im_code = vec![3,3,1107,-1,8,3,4,3,99];
+
+       assert_expected_output(&is_equal_to_8_pos_code, vec![8], vec![1]);
+       assert_expected_output(&is_equal_to_8_pos_code, vec![3], vec![0]);
+
+       assert_expected_output(&is_lt_8_pos_code, vec![7], vec![1]);
+       assert_expected_output(&is_lt_8_pos_code, vec![9], vec![0]);
+
+       assert_expected_output(&is_equal_to_8_im_code, vec![8], vec![1]);
+       assert_expected_output(&is_equal_to_8_im_code, vec![3], vec![0]);
+
+       assert_expected_output(&is_lt_8_im_code, vec![7], vec![1]);
+       assert_expected_output(&is_lt_8_im_code, vec![9], vec![0]);
+    }
+
+    #[test]
+    fn test_jumps() {
+        let pos_jump_code = vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9];
+        let im_jump_code = vec![3,3,1105,-1,9,1101,0,0,12,4,12,99,1];
+
+        assert_expected_output(&pos_jump_code, vec![10], vec![1]);
+        assert_expected_output(&im_jump_code, vec![0], vec![0]);
+    }
+
+    #[test]
+    fn test_jump_comparisons() {
+        let code = vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+                        1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+                        999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99];
+        assert_expected_output(&code, vec![7], vec![999]);
+        assert_expected_output(&code, vec![8], vec![1000]);
+        assert_expected_output(&code, vec![9], vec![1001]);
+    }
 }
